22144e883a30f7dde88865873aaa0c8c6a9d0aad
martingerardoc <u238513729@byui.edu>
2025-09-27 18:59:57 -0300

    join page created

--- scripts/data.js
+++ scripts/data.js
@@ -3,3 +3,10 @@ document.getElementById("year").textContent = new Date().getFullYear();
 
 // Setting the last modified date
 document.getElementById("lastModified").textContent = `Last Update: ${document.lastModified}`;
+
+ // Mobile nav toggle
+    const menuBtn = document.getElementById('menu-toggle');
+    const nav = document.getElementById('main-nav');
+    menuBtn.addEventListener('click', () => {
+      nav.classList.toggle('open');
+    });
