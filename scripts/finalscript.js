0456cec952495f66d45e28c85152a43fadd6b7c4
martingerardoc <u238513729@byui.edu>
2025-10-11 09:39:02 -0300

    final project modifications

--- scripts/finalscript.js
+++ scripts/finalscript.js
@@ -6,6 +6,14 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
 
+// Setting the current year in the footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Setting the last modified date
+document.getElementById(
+  "lastModified"
+).textContent = `Last Update: ${document.lastModified}`;
+
 // Carousel logic
 const carousels = document.querySelectorAll('.carousel');
 
